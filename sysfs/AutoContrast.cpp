87dd1b7137111dc0f06cb5d615b8ae2dec575902
Paul Keith <javelinanddart@gmail.com>
2019-02-07 09:57:05 -0600

    livedisplay: sysfs: Wire it up
    Change-Id: I8e8e222f94bcf86d4ded3d8dc57cc6c70ee7634a
    
    # Conflicts:
    #	sysfs/AdaptiveBacklight.cpp
    #	sysfs/AdaptiveBacklight.h
    #	sysfs/Android.bp
    #	sysfs/AutoContrast.cpp
    #	sysfs/AutoContrast.h
    #	sysfs/ColorEnhancement.cpp
    #	sysfs/ColorEnhancement.h
    #	sysfs/DisplayColorCalibration.cpp
    #	sysfs/DisplayColorCalibration.h
    #	sysfs/ReadingEnhancement.cpp
    #	sysfs/ReadingEnhancement.h
    #	sysfs/SunlightEnhancement.cpp
    #	sysfs/SunlightEnhancement.h

--- sysfs/AutoContrast.cpp
+++ sysfs/AutoContrast.cpp
@@ -1,30 +1,59 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/file.h>
+#include <android-base/strings.h>
+
+#include <fstream>
+
 #include "AutoContrast.h"
+
+using android::base::ReadFileToString;
+using android::base::Trim;
+using android::base::WriteStringToFile;
 
 namespace vendor {
 namespace lineage {
 namespace livedisplay {
 namespace V2_0 {
-namespace implementation {
+namespace sysfs {
+
+bool AutoContrast::isSupported() {
+    std::fstream aco(FILE_ACO, aco.in | aco.out);
+
+    return aco.good();
+}
 
 // Methods from ::vendor::lineage::livedisplay::V2_0::IAutoContrast follow.
 Return<bool> AutoContrast::isEnabled() {
-    // TODO implement
-    return bool {};
+    std::string tmp;
+    int32_t contents = 0;
+
+    if (ReadFileToString(FILE_ACO, &tmp)) {
+        contents = std::stoi(Trim(tmp));
+    }
+
+    return contents > 0;
 }
 
 Return<bool> AutoContrast::setEnabled(bool enabled) {
-    // TODO implement
-    return bool {};
+    return WriteStringToFile(enabled ? "1" : "0", FILE_ACO, true);
 }
 
-
-// Methods from ::android::hidl::base::V1_0::IBase follow.
-
-//IAutoContrast* HIDL_FETCH_IAutoContrast(const char* /* name */) {
-    //return new AutoContrast();
-//}
-//
-}  // namespace implementation
+}  // namespace sysfs
 }  // namespace V2_0
 }  // namespace livedisplay
 }  // namespace lineage
--- sysfs/ColorEnhancement.cpp
+++ sysfs/ColorEnhancement.cpp
@@ -1,30 +1,59 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/file.h>
+#include <android-base/strings.h>
+
+#include <fstream>
+
 #include "ColorEnhancement.h"
+
+using android::base::ReadFileToString;
+using android::base::Trim;
+using android::base::WriteStringToFile;
 
 namespace vendor {
 namespace lineage {
 namespace livedisplay {
 namespace V2_0 {
-namespace implementation {
+namespace sysfs {
+
+bool ColorEnhancement::isSupported() {
+    std::fstream ce(FILE_CE, ce.in | ce.out);
+
+    return ce.good();
+}
 
 // Methods from ::vendor::lineage::livedisplay::V2_0::IColorEnhancement follow.
 Return<bool> ColorEnhancement::isEnabled() {
-    // TODO implement
-    return bool {};
+    std::string tmp;
+    int32_t contents = 0;
+
+    if (ReadFileToString(FILE_CE, &tmp)) {
+        contents = std::stoi(Trim(tmp));
+    }
+
+    return contents > 0;
 }
 
 Return<bool> ColorEnhancement::setEnabled(bool enabled) {
-    // TODO implement
-    return bool {};
+    return WriteStringToFile(enabled ? "1" : "0", FILE_CE, true);
 }
 
-
-// Methods from ::android::hidl::base::V1_0::IBase follow.
-
-//IColorEnhancement* HIDL_FETCH_IColorEnhancement(const char* /* name */) {
-    //return new ColorEnhancement();
-//}
-//
-}  // namespace implementation
+}  // namespace sysfs
 }  // namespace V2_0
 }  // namespace livedisplay
 }  // namespace lineage
